93f412a70288a025b60daf7aff773b6e92ff5eea
Max Kurii <kuriimaxym@gmail.com>
2020-10-31 19:33:47 +0300

    changed the cycle

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 
-function createArray(size){
+function createArray(length){
   let arr =[];
-  for(let i = 0; i < size; i++){
+  for(let i = 0; i < length; i++){
     arr.push(i);
+    console.log(arr);
     
     if(i % 3 === 0){
       console.log('foo' +' '+ i );
-    }
+    }else{
       if(i % 5 === 0){
         console.log('bar' +' '+ i );
-    }
+    }else{
       if(i % 3 === 0 && i % 5 === 0){
         console.log(' FooBazz'  +' '+ i );
         }
+    }
+    }
+      
+      
   }
 }
-createArray(10);
+createArray(17);
